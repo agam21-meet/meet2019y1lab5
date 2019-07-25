2a0063af12451e5d3f22bea8f69000c417afd125
agam21-meet <agam21@meet.mit.edu>
2019-07-25 03:34:39 -0400

    Stopping here

---
+++ lab_5.py
@@ -0,0 +1,15 @@
+list_1=[1,2,3]
+list_2=[2,2,3]
+def list_match(list_one,list_two):
+    list3=[]
+    if list_1[0]==list_2[0]:
+        list_3[0]=list_1[0]
+    if list_1[1]==list_2[1]:
+        list_3[1]=list_1[1]
+    if list_1[2]==list_2[2]:
+        list_3[2]=list_1[2]
+    print(list3)
+
+
+
+
